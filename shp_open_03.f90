6d83f34139dacc82f3d76d58dad2acd5b2917781
Lionel GUEZ <guez@lmd.ens.fr>
2022-11-08 15:24:07 +0100

    Add iostat argument to `shp_open_03`

--- shp_open_03.f90
+++ shp_open_03.f90
@@ -4,7 +4,7 @@ module shp_open_03_m
 
 contains
 
-  subroutine shp_open_03(hshp, pszshapefile, pszaccess)
+  subroutine shp_open_03(hshp, pszshapefile, pszaccess, iostat)
 
     use shapelib, only: shpfileobject, shpopen, shpfileisnull, dbffileisnull
 
@@ -14,15 +14,23 @@ contains
     CHARACTER(len=*), INTENT(in):: pszaccess ! File access mode.
     ! Should be "rb" for reading or "rb+" for updating a file.
 
+    integer, optional, intent(out):: iostat
+
     !-------------------------------------------------------------
 
     hshp = shpopen(pszshapefile, pszaccess)
     
     if (shpfileisnull(hshp) .or. dbffileisnull(hshp)) then
-       print *, "shp_open_03: error"
-       print *, "pszshapefile = ", pszshapefile
-       print *, "pszaccess = ", pszaccess
-       stop 1
+       if (present(iostat)) then
+          iostat = 1
+       else
+          print *, "shp_open_03: error"
+          print *, "pszshapefile = ", pszshapefile
+          print *, "pszaccess = ", pszaccess
+          stop 1
+       end if
+    else
+       if (present(iostat)) iostat = 0
     end if
     
   end subroutine shp_open_03
